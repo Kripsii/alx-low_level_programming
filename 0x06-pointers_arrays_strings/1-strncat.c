a79da495951100a71a32f677999bf2ee48d8a392
kripsii <alekimaina64@gmail.com>
2022-03-24 15:45:45 -0700

    README.md

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,12 +4,13 @@
  * @dest: destination string.
  * @src: source string.
  * @n: an input integer.
+ *
  * Return: a pointer to resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
-       	int j = 0;
+	int j = 0;
 
 	while (*(dest + i))
 	{
@@ -18,9 +19,9 @@ char *_strncat(char *dest, char *src, int n)
 
 	while ((src + j < src + n) && *(src + j) != '\0')
 	{
-	*(dest + i) = *(src + j);
-	i++;
-	j++;
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
 	}
 	
 	return (dest);
