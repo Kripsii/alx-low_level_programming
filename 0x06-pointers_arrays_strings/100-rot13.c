74371b431dd1d63f788117fec2cf9f9911dc5023
kripsii <alekimaina64@gmail.com>
2022-03-25 14:45:47 -0700

    README.md

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * rot13 -  a function that encodes a string using rot13.
  * @s: An input string to encode using rot13
@@ -22,9 +23,11 @@ char *rot13(char *s)
 
 			i++;
 		}
+
 		i++;
 
 	}
+
 	return (s);
 
 }
