a7d360de85f79448bc89a92a866a7585dcd5a021
kripsii <alekimaina64@gmail.com>
2022-03-25 14:25:12 -0700

    README.md

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,17 +15,17 @@ char *cap_string(char *s)
 	{
 		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] -= 32;
+
 		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			s[i + 1] -= 32;
 
-			i++;
+		i++;
 	}
-
 	return (s);
 }
 
 /**
- * check_seperators - Seperators of words: space, tabulation, new line,
+ * check_seperators - Separators of words: space, tabulation, new line
  * ,, ;, ., !, ?, ", (, ), {, and }
  * @c: an input character
  * Return: 1 if seperator, 0 otherwise
@@ -37,8 +37,10 @@ int check_seperators(char c)
 		'"', '(', ')', '{', '}' };
 	for (; i < 13; i++)
 	{
+
 		if (c == seperators[i])
 			return (1);
 	}
 	return (0);
 }
+
