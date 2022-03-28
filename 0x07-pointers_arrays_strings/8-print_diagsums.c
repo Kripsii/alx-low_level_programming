fa2f46b87751e6619b6793cbd3a4eba38a2da2f6
kripsii <alekimaina64@gmail.com>
2022-03-28 12:16:44 -0700

    README.md

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,29 +9,23 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int bdiagsum = 0;
-	int fdiagsum = 0;
-	int prev = 0;
+	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i < size * size; i++)
-	{
-		if (i == 0)
-		{
-			 bdiagsum += *(a + i);
-			 prev = i;
-
-		}
-		else if (i == (prev + size + 1))
-		{
-			bdiagsum += *(a + i);
-			prev = i;
-		}
-
-	}
 	for (i = 0; i < size; i++)
 	{
-		fdiagsum += *(a + (size * (i + 1) - (i + 1)));
+		sum1 += a[i];
+		a += size;
 	}
-	printf("%d, %d\n", bdiagsum, fdiagsum);
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+
 }
+
