a1812f8b1ca6395af5a47ba9f919a4e897d519ec
kripsii <alekimaina64@gmail.com>
2022-04-05 15:53:25 -0700

    README.md

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a 2D integer grid
+ * @grid: The grid to be freed.
+ * @height:height of the grid
+ * Return: Nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
