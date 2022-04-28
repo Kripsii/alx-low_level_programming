c2301e15b66785e3200dbe19a956fd21d57aa416
kripsii <alekimaina64@gmail.com>
2022-04-28 10:53:03 -0700

    README.md

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,63 +1,24 @@
 #include "main.h"
 
 /**
- * str_len - Finds length of string.
- * @s: string for which length to be found
- * Return: Lenght of string
- */
-int str_len(const char *s)
-{
-	int len = 0, idx;
-
-	for (idx = 0; *(s + idx); idx++)
-	{
-		if (*(s + idx) == '\0')
-			break;
-		len++;
-	}
-	return (len);
-}
-
-/**
- * power - Finds Power @y of @x.
- * @x: base number
- * @y: exponent
- * Return: x to power of y
- */
-int power(int x, int y)
-{
-	int pow = 0;
-
-	if (y == 0)
-		return (1);
-
-	pow = x * power(x, y - 1);
-	return (pow);
-}
-
-/**
- * binary_to_uint - Converts binary number to unsigned int.
- * @b: pointer to string 0 and 1
- * Return: The converted number, 0 if b is NULL.
- * 0 if there's one or more chars in b != 0 or 1.
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len = str_len(b);
-	unsigned int dec = 0, idx = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	while (len > 0)
+	for (i = 0; b[i]; i++)
 	{
-		--len;
-		if (*(b + len) != '0' && *(b + len) != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		dec += (*(b + len) - '0') * power(2, idx);
-		idx++;
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (dec);
-}
 
+	return (dec_val);
+}
