4b0eb02850bf290361680a3f4b5e1b5dbaa25ee4
Kripsii <alekimaina64@gmail.com>
2022-08-20 05:22:30 -0700

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,10 +9,10 @@ def island_perimeter(grid):
             if item == 1:
                 if x == 0 or grid[y][x - 1] == 0:
                     perim += 1
-                    if x == len(row) - 1 or grid[y][x + 1] == 0:
+                if x == len(row) - 1 or grid[y][x + 1] == 0:
                         perim += 1
-                        if y == 0 or grid[y - 1][x] == 0:
+                if y == 0 or grid[y - 1][x] == 0:
                             perim += 1
-                            if y == len(grid) - 1 or grid[y + 1][x] == 0:
+                if y == len(grid) - 1 or grid[y + 1][x] == 0:
                                 perim += 1
-                                return perim
+    return perim
